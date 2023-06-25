22bcfff741f1c57a34119a9f25efe8d48da7a080
Napster <rome.1205h@gmail.com>
2023-06-24 20:37:20 -0500

    add header and nuevoPresu

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import Header from './components/Header'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>Control de gastos</h1>
+    <Header/>
   )
 }
 
