eb1450bdf01e2fee12284215a9c9ccf7e80af420
Napster <rome.1205h@gmail.com>
2023-06-25 23:59:38 -0500

    add text componentente nnuevo

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function NuevoPresupuesto() {
    return (
-      <div>NuevoPresupuesto</div>
+      <div className='contenedor-presupuesto contenedor sombra'>
+         <form className='formulario'>
+            <div className='campo'>
+               <label>Definir presupuesto</label>
+               <input
+                  className='nuevo-presupuesto'
+                  type="text"
+                  placeholder='Añade tu presupuesto'
+               />
+            </div>
+            <input type="submit" value="Añadir"/>
+         </form>
+      </div>
    )
 }
 
