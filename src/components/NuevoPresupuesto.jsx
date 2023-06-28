5d046f743796b094ec405c5c67ac608090bec350
Napster <rome.1205h@gmail.com>
2023-06-27 23:57:01 -0500

    add mensaje de error

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Mensaje from './Mensaje'
 
 function NuevoPresupuesto({presupuesto, setPresupuesto}) {
+
+   const [ mensaje, setMensaje ] = useState('')
+
+   const handlePresupuesto = (e) => {
+      e.preventDefault()
+      
+      if(!Number(presupuesto) || Number(presupuesto) < 0 ){
+         setMensaje("No es un presupuesto valido")
+      } else {
+         console.log("Si es un presupuesto valido")
+      }
+   }
+
    return (
       <div className='contenedor-presupuesto contenedor sombra'>
-         <form className='formulario'>
+         <form onSubmit={handlePresupuesto} className='formulario'>
             <div className='campo'>
                <label>Definir presupuesto</label>
                <input
@@ -15,6 +29,8 @@ function NuevoPresupuesto({presupuesto, setPresupuesto}) {
                />
             </div>
             <input type="submit" value="Añadir"/>
+
+            {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
          </form>
       </div>
    )
