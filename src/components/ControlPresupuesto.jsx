6e5e237f1376e6a0ae6232db2d9585bf3a3babb6
Napster <rome.1205h@gmail.com>
2023-07-04 00:15:17 -0500

    add formato currency USD al presu

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 
 const  ControlPresupuesto = ({presupuesto}) => {
+
+   const formatearCant = (cantidad) => {
+      return cantidad.toLocaleString('en-US', {
+         style: 'currency',
+         currency: 'USD'
+      })
+   }
+
+
    return (
       <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
          <div>
@@ -8,7 +17,13 @@ const  ControlPresupuesto = ({presupuesto}) => {
          </div>
          <div className='contenido-presupuesto'>
             <p>
-               <span>Presupuesto: </span> ${presupuesto}
+               <span>Presupuesto: </span> {formatearCant(presupuesto)}
+            </p>
+            <p>
+               <span>Disponible: </span> {formatearCant(0)}
+            </p>
+            <p>
+               <span>Gastado: </span> {formatearCant(0)}
             </p>
          </div>
       </div>
