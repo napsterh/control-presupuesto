8d4c88c574d27c2b9ab636e11c0f3724838a7282
Napster <rome.1205h@gmail.com>
2023-07-07 23:55:37 -0500

    106 anadiendo form de gastos en modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -19,6 +19,41 @@ const Modal = ({setModal, animarModal}) => {
 
          <form className={`formulario ${animarModal ? "animar" : '' }`}>
             <legend>Nuevo gasto</legend>
+            <div className='campo'>
+               <label htmlFor='nombre'>Nombre Gasto</label>
+               <input
+                  id='nombre'
+                  type="text"
+                  placeholder='Anade el nombre del gasto'
+               />
+            </div>
+            <div className='campo'>
+               <label htmlFor='cantidad'>Cantidad</label>
+               <input
+                  id='cantidad'
+                  type="number"
+                  placeholder='Anade la cantidad del gasto: ej. 300'
+               />
+            </div>
+            <div className='campo'>
+               <label htmlFor='categoria'>Categoria</label>
+               <select
+                  id='categoria'
+               >
+                  <option value="">-- Seleccione --</option>
+                  <option value="ahorro">Ahorro</option>
+                  <option value="comida">Comida</option>
+                  <option value="casa">Casa</option>
+                  <option value="gastos">Gastos varios</option>
+                  <option value="ocio">Ocio</option>
+                  <option value="salud">Salud</option>
+                  <option value="Suscripciones">Suscripciones</option>
+               </select>
+            </div>
+            <input
+               type="submit"
+               value="Anadir gastos"
+            />
          </form>
       </div>
    )
