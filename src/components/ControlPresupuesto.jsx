2154c01c461d14acf43cb056b45d64fdf0550363
Napster <rome.1205h@gmail.com>
2023-08-01 23:54:36 -0500

    123 add grafica circular

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,4 +1,6 @@
 import React, {useEffect, useState} from 'react'
+import { CircularProgressbar } from 'react-circular-progressbar'
+import "react-circular-progressbar/dist/styles.css"
 
 const  ControlPresupuesto = ({gastos, presupuesto}) => {
 
@@ -25,7 +27,9 @@ const  ControlPresupuesto = ({gastos, presupuesto}) => {
    return (
       <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
          <div>
-            <p>Grafico aqui</p>
+            <CircularProgressbar
+               value={50}
+            />
          </div>
          <div className='contenido-presupuesto'>
             <p>
