2060bb6835fac1293adf68dedec651a72d24a66c
Napster <rome.1205h@gmail.com>
2023-08-09 00:04:26 -0500

    130 gastos filtrados en state

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ function App() {
   const [ gastoEditar, setGastoEditar ] = useState({})
 
   const [ filtro, setFiltro ] = useState('')
+  const [ gastosFiltrados, setGastosFiltrados ] = useState([])
 
   useEffect(() => {
     if( Object.keys(gastoEditar).length > 0 ){
@@ -44,7 +45,9 @@ function App() {
   
   useEffect(() => {
     if(filtro){
-      console.log('filtrando..', filtro)
+      const gastosFiltrados = gastos.filter(  gasto => gasto.categoria === filtro )
+
+      setGastosFiltrados(gastosFiltrados)
     }
   }, [filtro])
   
